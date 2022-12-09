edadfc102238f737ec46821486beddd9a43aba13
chaudhry0 <h.chaudhrymohammad@studenti.unitn.it>
2022-12-09 16:03:36 +0100

    snake added

---
+++ Code/Games/snake.c
@@ -0,0 +1,187 @@
+#include "snake.h"
+#include "Code/Images/snakeImages.h"
+
+void initSnakeBody(List* snakeBody){
+    initList(snakeBody);
+    int i = 0;
+    for (i=0; i<INITIAL_LENGTH; i++){
+        push(snakeBody, i+1, CELL_SIZE_SMALL - 1);
+    }
+}
+
+bool checkIfOpposite(enum Moving moving) {
+    if ((moving == RIGHT && lastMove == 4) ||
+        (moving == LEFT && lastMove == 2) ||
+        (moving == DOWN && lastMove == 1) ||
+        (moving == UP && lastMove == 3)) {
+        return true;
+    }
+    return false;
+}
+
+void moveSnake(List* snakeBodyPtr, enum Moving* moving, int xHead, int yHead, int* lastXPtr, int* lastYPtr){
+    int xVel = 0;
+    int yVel = 0;
+    if (!checkIfOpposite(*moving)){
+        switch(lastMove){
+            case 1:
+                *moving = UP;
+                break;
+            case 2:
+                *moving = RIGHT;
+                break;
+            case 3:
+                *moving = DOWN;
+                break;
+            case 4:
+                *moving = LEFT;
+                break;
+            default:
+                break;
+        }
+    }
+    switch(*moving){
+        case UP:
+            yVel = -1;
+            xVel = 0;
+            break;
+        case RIGHT:
+            xVel = 1;
+            yVel = 0;
+            break;
+       case DOWN:
+           yVel = 1;
+           xVel = 0;
+           break;
+       case LEFT:
+           xVel = -1;
+           yVel = 0;
+           break;
+       default:
+           break;
+    }
+    Cords cordsHead = getFirstElem(*snakeBodyPtr);
+    *lastXPtr = cordsHead.x;
+    *lastYPtr = cordsHead.y;
+    push(snakeBodyPtr, xHead + xVel, yHead + yVel);
+}
+
+void drawSnakeBackground() {
+    Graphics_drawImage(&g_sContext, &imageSnakeBackground, 0, CELL_SIZE_SMALL * 2);
+}
+
+void clearSnake(int tailx, int taily, bool* lightGreenCell){
+    if (*lightGreenCell){
+        drawRect(CELL_SIZE_SMALL * tailx,
+                 CELL_SIZE_SMALL * tailx + CELL_SIZE_SMALL -1,
+                 CELL_SIZE_SMALL * taily,
+                 CELL_SIZE_SMALL * taily + CELL_SIZE_SMALL -1,
+                 0x74a47c);
+    }else {
+        drawRect(CELL_SIZE_SMALL * tailx,
+                 CELL_SIZE_SMALL * tailx + CELL_SIZE_SMALL -1,
+                 CELL_SIZE_SMALL * taily,
+                 CELL_SIZE_SMALL * taily + CELL_SIZE_SMALL -1,
+                 0x2b933c);
+    }
+    *lightGreenCell = !(*lightGreenCell);
+}
+
+void drawSnakeCell(int x, int y) {
+    Graphics_drawImage(&g_sContext, &imageSnakeBody, x * CELL_SIZE_SMALL, y * CELL_SIZE_SMALL);
+}
+
+void drawSnakeFull(List snakeBody) {
+    int i;
+    for (i=0; i<snakeBody.length; i++){
+        drawSnakeCell(getElem(snakeBody, i + 1).x, getElem(snakeBody, i + 1).y);
+    }
+}
+
+void generateRandomApple(List snakeBody, Apple* apple) {
+    apple->x = random(0, 15);
+    apple->y = random(2, 13);
+    int i;
+    for (i=0; i<snakeBody.length; i++){
+        if (apple->x == getElem(snakeBody, snakeBody.length - i).x && apple->y == getElem(snakeBody, snakeBody.length - i).y){
+            generateRandomApple(snakeBody, apple);
+        }
+    }
+}
+
+void drawApple(Apple apple) {
+    Graphics_drawImage(&g_sContext, &imageSnakeApple, apple.x * CELL_SIZE_SMALL, apple.y * CELL_SIZE_SMALL);
+}
+
+bool checkAppleCollision(List snakeBody, Apple apple) {
+    if (apple.x == getLastElem(snakeBody).x && apple.y == getLastElem(snakeBody).y){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+bool checkBorderCollision(List snakeBody) {
+    if (getLastElem(snakeBody).x < 0 || getLastElem(snakeBody).x > 15 || getLastElem(snakeBody).y < 2 || getLastElem(snakeBody).y > 13){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+bool checkSnakeCollision(List snakeBody) {
+    int i = 0;
+    for (i=3; i<=snakeBody.length-1; i++){
+        if ((getLastElem(snakeBody).x == getElem(snakeBody, snakeBody.length - i).x) &&
+           (getLastElem(snakeBody).y == getElem(snakeBody, snakeBody.length - i).y)){
+            return true;
+        }
+    }
+    return false;
+}
+
+void drawSnakeInitial() {
+    Graphics_drawImage(&g_sContext, &imageSnakeTitle, 0, 16);
+    wait(TIME_TITLE_GAME);
+}
+
+void runSnake() {
+    bool lightGreenCell = true;
+    int speed = 300;
+    int lastX = 0;
+    int lastY = 0;
+    lastMove = 0;
+    List snakeBody;
+    Apple apple;
+    enum Moving moving = RIGHT;
+    drawSnakeInitial();
+    initSnakeBody(&snakeBody);
+    generateRandomApple(snakeBody, &apple);
+    drawSnakeBackground();
+    drawSnakeFull(snakeBody);
+    drawApple(apple);
+    drawScoreText();
+    drawScore(score);
+
+    while(!gameOver){
+        printf("%d\n", score);
+        moveSnake(&snakeBody, &moving, getLastElem(snakeBody).x, getLastElem(snakeBody).y, &lastX, &lastY);
+        if (checkBorderCollision(snakeBody) || checkSnakeCollision(snakeBody) || score == 100){
+            gameOver = true;
+        } else{
+            if (!checkAppleCollision(snakeBody, apple)){
+                pop(&snakeBody);
+                clearSnake(lastX, lastY, &lightGreenCell);
+            } else{
+                generateRandomApple(snakeBody, &apple);
+                drawApple(apple);
+                drawScore(++score);
+                if (speed > 100){
+                    speed -= 5;
+            }
+        }
+        drawSnakeCell(getLastElem(snakeBody).x, getLastElem(snakeBody).y);
+        wait(speed);
+        }
+    }
+}
---
+++ Code/Games/snake.h
@@ -0,0 +1,40 @@
+#ifndef SNAKE_H_
+#define SNAKE_H_
+
+#include "Code/Utilities/init.h"
+#include "Code/Utilities/list.h"
+
+#define INITIAL_LENGTH 3
+#define SNAKE_SPEED 200
+
+enum Moving {UP, RIGHT, DOWN, LEFT};
+
+typedef struct Apple {
+    int x;
+    int y;
+} Apple;
+
+
+    // [FUNCTIONS]
+// init functions
+void initSnakeBody(List* snakeBody); // initializes snake body (after clearing it)
+// moving/updating functions
+void moveSnake(List* snakeBodyPtr, enum Moving* moving, int xHead, int yHead, int* lastXPtr, int* lastYPtr); // updates snake xVel and yVel; then it pushes the tail to the head (updating cords)
+void generateRandomApple(List snakeBody, Apple* apple); // generate new apple cords avoiding overlaps with the snake
+// bool functions
+bool checkIfOpposite(enum Moving moving); // checks if the user prompts a not allowed movement
+bool checkAppleCollision(List snakeBody, Apple apple); // checks if the apple is eaten
+bool checkBorderCollision(List snakeBody); // checks if snake collides with the border
+bool checkSnakeCollision(List snakeBody); // checks if snake collides with itself
+// draw functions
+void drawSnakeInitial(); // draws initial text
+void drawSnakeBackground(); // draws the background
+void drawSnakeCell(int x, int y); // draws a single snake body cell
+void drawSnakeFull(List snakeBody); // draws the full snake body
+void clearSnake(int tailx, int taily, bool* lightGreenCell); // clears the old background cell after snake movements
+void drawApple(); // draw the apple
+// run function
+void runSnake(); // runs the Snake game
+
+
+#endif /* SNAKE_H_ */
