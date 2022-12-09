d160bfc950d39177c3830e0cf8846c09e58e94d3
chaudhry0 <h.chaudhrymohammad@studenti.unitn.it>
2022-12-09 16:04:56 +0100

    rhinoRunner added

---
+++ Code/Games/rhinoRunner.c
@@ -0,0 +1,162 @@
+#include "rhinoRunner.h"
+#include "Code/Images/rhinoImages.h"
+
+void drawInitialRhino(){
+    Graphics_drawImage(&g_sContext, &imagetitleRhino, 0, CELL_SIZE_LARGE);
+    wait(TIME_TITLE_GAME);
+}
+
+void runRhino() {
+    prova = 0;
+    Rhino rhino;
+    int counterRhino;
+    Obstacle obstacle1;
+    Obstacle obstacle2;
+    initRhino(&rhino, &counterRhino);
+    drawInitialRhino();
+    drawRhinoBackground();
+    drawRhino(rhino);
+    drawScoreText();
+    drawScore(score);
+    initObstacle(&obstacle1, 0);
+    initObstacle(&obstacle2, 80);
+
+    while(!gameOver){
+        updateRhino(&rhino, &counterRhino);
+        clearRhino(rhino);
+        drawRhino(rhino);
+        updateObstacle(&obstacle1);
+        updateObstacle(&obstacle2);
+        clearObstacle(obstacle1);
+        clearObstacle(obstacle2);
+        drawObstacle(obstacle1);
+        drawObstacle(obstacle2);
+
+        if (checkRhinoCollision(rhino, obstacle1) || checkRhinoCollision(rhino, obstacle2)){
+            gameOver = true;
+        }
+        if (checkIfObstacleEnds(obstacle1) && obstacle2.x < 128 - 80){
+            initObstacle(&obstacle1, 0);
+            drawScore(++score);
+        }
+        if (checkIfObstacleEnds(obstacle2) && obstacle1.x < 128 - 80){
+            initObstacle(&obstacle2, 0);
+            drawScore(++score);
+        }
+        wait(20);
+    }
+}
+
+void initRhino(Rhino* rhinoPtr, int* counterRhinoPtr) {
+    *counterRhinoPtr = 0;
+    rhinoPtr->x = CELL_SIZE_LARGE;
+    rhinoPtr->y = MAX_HEIGHT - RHINO_Y_SIZE;
+    rhinoPtr->action = NONE;
+}
+
+
+
+void initObstacle(Obstacle* obstaclePtr, int distance) {
+    if (random(0, 2) == 2){
+        obstaclePtr->type = BIRD;
+        obstaclePtr->x = MAX_WIDTH - BIRD_X_SIZE + 1 + distance;
+        obstaclePtr->y = MAX_HEIGHT - BIRD_HEIGHT - BIRD_Y_SIZE;
+    } else{
+        obstaclePtr->type = CACTUS;
+        obstaclePtr->x = MAX_WIDTH - CACTUS_X_SIZE + 1 + distance;
+        obstaclePtr->y = MAX_HEIGHT - CACTUS_Y_SIZE;
+    }
+}
+
+void drawRhinoBackground() {
+    Graphics_drawImage(&g_sContext, &imageRinoBackground, 0, CELL_SIZE_LARGE);
+}
+
+void drawRhino(Rhino rhino) {
+    Graphics_drawImage(&g_sContext, &imageRino, rhino.x, rhino.y);
+
+    if (prova == 1){
+        imageRino.pPixel = pixelRino2;
+        prova = 0;
+    } else{
+        imageRino.pPixel = pixelRino;
+        prova++;
+    }
+}
+
+void clearRhino(Rhino rhino) {
+    if (rhino.action == NONE){
+        drawRect(rhino.x, rhino.x + RHINO_X_SIZE - 1, rhino.y - 1, rhino.y - GAME_SPEED, 0xffffff);
+    }else if (rhino.action == GOING_UP){
+        drawRect(rhino.x, rhino.x + RHINO_X_SIZE - 1, rhino.y + RHINO_Y_SIZE, rhino.y + RHINO_Y_SIZE  + GAME_SPEED - 1, 0xffffff);
+    }else if (rhino.action == GOING_DOWN){
+        drawRect(rhino.x, rhino.x + RHINO_X_SIZE - 1, rhino.y + 1, rhino.y - GAME_SPEED, 0xffffff);
+    }
+}
+
+void updateRhino(Rhino* rhinoPtr, int* counterRhinoPtr){
+    if (rhinoPtr->action == NONE && getButtons() == 1){
+        rhinoPtr->action = GOING_UP;
+    }
+    if (rhinoPtr->action == GOING_UP){
+        rhinoPtr->y -= GAME_SPEED;
+        if (++(*counterRhinoPtr) == JUMP_COUNTER || getButtons() == 0){
+            rhinoPtr->action = GOING_DOWN;
+        }
+    }
+    if (rhinoPtr->action == GOING_DOWN){
+        rhinoPtr->y += GAME_SPEED;
+        if (--(*counterRhinoPtr) == 0){
+            rhinoPtr->action = NONE;
+        }
+    }
+}
+
+void drawObstacle(Obstacle obstacle) {
+    if(obstacle.type == CACTUS){
+        Graphics_drawImage(&g_sContext, &imageCactus, obstacle.x, obstacle.y);
+    }else{
+        Graphics_drawImage(&g_sContext, &imageBird, obstacle.x, obstacle.y);
+    }
+}
+
+void clearObstacle(Obstacle obstacle) {
+    if(obstacle.type == CACTUS){
+        drawRect(obstacle.x + CACTUS_X_SIZE, obstacle.x + CACTUS_X_SIZE + GAME_SPEED - 1, MAX_HEIGHT-CACTUS_Y_SIZE, MAX_HEIGHT-1, 0xffffff);
+    }else{
+        drawRect(obstacle.x + BIRD_X_SIZE, obstacle.x + BIRD_X_SIZE + GAME_SPEED - 1, MAX_HEIGHT - BIRD_HEIGHT - BIRD_Y_SIZE, MAX_HEIGHT - BIRD_HEIGHT - 1, 0xffffff);
+    }
+}
+
+bool checkIfObstacleEnds(Obstacle obstacle) {
+    if (obstacle.x <= 0 - CACTUS_X_SIZE && obstacle.type == CACTUS){
+        return true;
+    }else if (obstacle.x <= 0 - BIRD_X_SIZE && obstacle.type == BIRD){
+        return true;
+    }
+    return false;
+}
+
+bool checkYCollision(Rhino rhino, Obstacle obstacle) {
+    if (obstacle.type == CACTUS){
+        if(rhino.y + RHINO_Y_SIZE >= obstacle.y){
+            return true;
+        }
+    }else if(rhino.y <= obstacle.y + BIRD_HEIGHT){
+        return true;
+    }
+    return false;
+}
+
+bool checkRhinoCollision(Rhino rhino, Obstacle obstacle) {
+    if (rhino.x <= obstacle.x && obstacle.x <= rhino.x + RHINO_X_SIZE){
+        if(checkYCollision(rhino, obstacle)){
+            return true;
+        }
+    }
+    return false;
+}
+
+void updateObstacle(Obstacle* obstaclePtr) {
+    obstaclePtr->x -= GAME_SPEED;
+}
---
+++ Code/Games/rhinoRunner.h
@@ -0,0 +1,60 @@
+#ifndef CODE_GAMES_RHINORUNNER_H_
+#define CODE_GAMES_RHINORUNNER_H_
+
+#include "Code/Utilities/init.h"
+
+#define JUMP_COUNTER 11
+
+#define RHINO_X_SIZE 24
+#define RHINO_Y_SIZE 12
+
+#define CACTUS_X_SIZE 8
+#define CACTUS_Y_SIZE 16
+
+#define BIRD_X_SIZE 16
+#define BIRD_Y_SIZE 14
+#define BIRD_HEIGHT 20
+
+#define GAME_SPEED 4
+
+enum Type {CACTUS, BIRD};
+enum Action {NONE, GOING_UP, GOING_DOWN};
+
+typedef struct Rhino{
+    int x;
+    int y;
+    enum Action action;
+} Rhino;
+
+typedef struct Obstacle{
+    int x;
+    int y;
+    enum Type type;
+} Obstacle;
+
+int prova;
+
+
+// [FUNCTIONS]
+// init functions
+void initRhino(Rhino* rhinoPtr, int* counterRhinoPtr); // initializes the rhino
+void initObstacle(Obstacle* obstaclePtr, int distance); // initializes an obstacle
+// moving/updating functions
+void updateRhino(Rhino* rhinoPtr, int* counterRhinoPtr); // updates rhino position
+void updateObstacle(Obstacle* obstaclePtr); // updates an obstacle
+// bool functions
+bool checkIfObstacleEnds(Obstacle obstacle); // checks if an obstacle has reached the end of the screen
+bool checkYCollision(Rhino rhino, Obstacle obstacle); // checks if the rhino collides vertically with an obstacle
+bool checkRhinoCollision(Rhino rhino, Obstacle obstacle); // checks if the rhino collides with an obstacle
+// draw functions
+void drawInitialRhino(); // draws initial text
+void drawRhinoBackground(); // draws the background
+void drawRhino(Rhino rhino); // draws rhino
+void clearRhino(Rhino rhino); // clears rhino
+void drawObstacle(Obstacle obstacle); // draws an obstacle
+void clearObstacle(Obstacle obstacle); // clears an obstacle
+// run function
+void runRhino(); // runs the Rhino game
+
+
+#endif /* CODE_GAMES_RHINORUNNER_H_ */
