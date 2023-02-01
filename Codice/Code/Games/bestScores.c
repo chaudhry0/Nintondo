ac836e14f7e583801ff57e3f9b0823ae88f9d87e
chaudhry0 <h.chaudhrymohammad@studenti.unitn.it>
2023-02-01 14:48:59 +0100

    added template to bestScores.h

--- Codice/Code/Games/bestScores.c
+++ Codice/Code/Games/bestScores.c
@@ -1,6 +1,44 @@
+/*******************************************************************************
+* Title                 :   BEST SCORES
+* Filename              :   bestScores.c
+* Last Revision Date    :   01/02/2023
+* Notes                 :   None
+*******************************************************************************/
+/*******************************************************************************
+ * @file bestScores.c
+ *
+ * @brief This module shows the best scores screen.
+ *        It draws a table with the best score from every game.
+ *        It also shows the total score.
+ *        The screen remains until button A is clicked.
+ *        The best scores are saved in the bestScores array.
+ * 
+ * @par       
+ * COPYRIGHT NOTICE: (c) 2023 Nintondo. All rights reserved.
+*******************************************************************************/
+/******************************************************************************
+* Includes
+*******************************************************************************/
 #include "bestScores.h"
 #include "Code/Images/bestScoreImages.h"
 
+/******************************************************************************
+* Function Definitions
+*******************************************************************************/
+
+/*!
+ * @brief This Function is to draw the best scores table.
+ *        It draws the table and the total score.
+ *        The total score is the sum of the best scores.
+ *        If the total score is greater than 99, it is shown as 99.
+ *        The best scores are saved in the bestScores array.
+ *              
+ *
+ * @param[in] none
+ *
+ * 
+ * @return none --> void
+ */
 void drawScores(){
     int totalScore = 0;
     char value[10];
@@ -20,6 +58,16 @@ void drawScores(){
     Graphics_drawString(&g_sContext, (int8_t *) value, 3, 110, 76 + BAR_SIZE, TRANSPARENT_TEXT);
 }
 
+/*!
+ * @brief This Function is to run the best scores screen.
+ *        It draws the best scores table and the screen remains until button A is clicked.
+ *              
+ *
+ * @param[in] none
+ *
+ * 
+ * @return none --> void
+ */
 void runBestScores(){
     Graphics_drawImage(&g_sContext, &imageBestScores, 0, 16);
     drawScores();
@@ -29,3 +77,6 @@ void runBestScores(){
         }
     }
 }
+
+
+/*** end of file ***/
