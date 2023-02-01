c9508b8255d53274594be5ba6762d735c4a3b33e
chaudhry0 <h.chaudhrymohammad@studenti.unitn.it>
2023-02-01 14:43:58 +0100

    added template to bestScores.h

--- Codice/Code/Games/bestScores.h
+++ Codice/Code/Games/bestScores.h
@@ -1,8 +1,28 @@
+/*******************************************************************************
+* Title                 :   BEST SCORES
+* Filename              :   bestScores.h
+* Last Revision Date    :   01/02/2023
+* Notes                 :   None
+*******************************************************************************/
+/*******************************************************************************
+ * @file bestScores.h
+ *
+ * @brief This is the header file for the best scores screen.
+ * 
+ * @par       
+ * COPYRIGHT NOTICE: (c) 2023 Nintondo. All rights reserved.
+*******************************************************************************/
 #ifndef CODE_GAMES_BESTSCORES_H_
 #define CODE_GAMES_BESTSCORES_H_
 
+/******************************************************************************
+* Includes
+*******************************************************************************/
 #include "Code/Utilities/init.h"
 
+/******************************************************************************
+* Function Prototypes
+*******************************************************************************/
 // draw functions
 void drawScores(); // function of drawing a table with the best score from every game
 // run function
@@ -10,3 +30,5 @@ void runBestScores(); // runs drawScore and the score screen remains until butto
 
 
 #endif
+
+/*** End of File **************************************************************/
