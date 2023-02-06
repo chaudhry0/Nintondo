32660ac41eb77fdad573c8e017e0b76db4bf35d2
chaudhry0 <h.chaudhrymohammad@studenti.unitn.it>
2023-02-06 11:04:20 +0100

    added template to gameHub.c

--- Codice/Code/Menu/gameHub.c
+++ Codice/Code/Menu/gameHub.c
@@ -1,6 +1,40 @@
+/*******************************************************************************
+* Title                 :   GAME HUB
+* Filename              :   gameHub.c
+* Last Revision Date    :   06/02/2023
+* Notes                 :   None
+*******************************************************************************/
+/*******************************************************************************
+ * @file gameHub.c
+ *
+ * @brief This module contains the game hub.
+ *        It contains the runGameHub function that is the main function of the Ninotondo. (only function called in main.c)
+ *        It has all the logic to run the games contained in the menu.
+ *        It also draws the logo and the game over screen.
+ *        When the game is selected it cleans the display and calls the run function of the game.  
+ * 
+ * @par       
+* COPYRIGHT NOTICE: (c) 2023 Nintondo. All rights reserved.
+*******************************************************************************/
+/******************************************************************************
+* Includes
+*******************************************************************************/
 #include "Code/Menu/gameHub.h"
 #include "Code/Images/backgroundImages.h"
 
+/******************************************************************************
+* Function Definitions
+*******************************************************************************/
+
+/*!
+ * @brief This Function is to draw the logo.
+ *        It is initial screen of the game.
+ *
+ * @param[in] none
+ *
+ * 
+ * @return none --> void
+ */
 void drawLogo(){
     drawRect(0, MAX_WIDTH - 1, 0, BAR_SIZE + MAX_HEIGHT + BAR_SIZE - 1, WHITE);
     Graphics_drawImage(&g_sContext, &imageLogo, CELL_LARGE, BAR_SIZE + 36);
@@ -9,6 +43,15 @@ void drawLogo(){
     Graphics_drawImage(&g_sContext, &imageBackground, 0, 0);
 }
 
+/*!
+ * @brief This Function is to draw the game over screen.
+ *        It is called when the game is over.
+ *
+ * @param[in] none
+ *
+ * 
+ * @return none --> void
+ */
 void drawGameOver(){
     startLEDBlinking();
     Graphics_drawImage(&g_sContext, &imageGameOver, CELL_LARGE, BAR_SIZE + 32);
@@ -16,10 +59,30 @@ void drawGameOver(){
     stopLEDBlinking();
 }
 
+/*!
+ * @brief This Function is to clean the bottom bar.
+ *        It is called after the user has selected a game or when the game is over.
+ *
+ * @param[in] none
+ *
+ * 
+ * @return none --> void
+ */
 void cleanDisplayGame(){
     drawRect(0, MAX_WIDTH - 1, CELL_LARGE, MAX_HEIGHT + CELL_LARGE - 1, WHITE);
 }
 
+/*!
+ * @brief This Function is to run the game hub.
+ *        It is the main function of the game. (only this function is called in the main.c)
+ *        It calls the menu and then the game selected by the user.
+ *        It also updates the best score and draws the game over screen.
+ *
+ * @param[in] none
+ *
+ * 
+ * @return none --> void
+ */
 void runGameHub(){
     consumeButtonA();   // resets bottom A before the games appear
     runMenu();
@@ -67,3 +130,5 @@ void runGameHub(){
     }
     cleanDisplayGame();
 }
+
+/*** end of file ***/
